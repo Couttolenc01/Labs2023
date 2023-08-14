48a02b89a207c2a8d0d619aaf13cea2489353aac
Couttolenc01 <A01754959@tec.mx>
2023-08-14 08:03:47 -0600

    Ejercicio_1 de Java listo

--- laboratorios/labs.js
+++ laboratorios/labs.js
@@ -1,4 +1,4 @@
-function ejercicio_2(){
+function ejercicio_1(){
     // Generación de una tabla con números, cuadrados y cubos:
     var numeroStr = prompt("Ingresa un número");
     var numero = parseInt(numeroStr); // Convertir la cadena en un número
@@ -11,4 +11,4 @@ function ejercicio_2(){
     document.getElementById("ejercicio_1").innerHTML = "Respuesta al ejercicio 1";
 }
 
-ejercicio_2();
+ejercicio_1();
