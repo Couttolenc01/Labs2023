a1a84d6b14c0afd31dfc730fcf6d63adbfd6898b
Couttolenc01 <A01754959@tec.mx>
2023-08-17 12:36:19 -0600

    agregue comentarios acerca de fs

--- laboratorios/app.js
+++ laboratorios/app.js
@@ -1,7 +1,10 @@
 console.log("Hola desde node!");
 
+//fs es el modulo de filesystem de node, que sirve para acceder a los metodos para manipular el sistema de archivos
 const filesystem = require('fs');
 
+//writeFileSync es un metodo que sirve para escribir en un archivo de manera sincrona.
+//Por default, los metodos de node son asincronos
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
 console.log("Ya acabe de escribir el archivo!");
