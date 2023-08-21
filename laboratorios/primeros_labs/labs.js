8deaca3a61165d74c97d7d99a1d405e6eedc7242
Couttolenc01 <A01754959@tec.mx>
2023-08-20 19:30:51 -0600

    Ejercicio 3 del lab 4, listo

--- laboratorios/primeros_labs/labs.js
+++ laboratorios/primeros_labs/labs.js
@@ -5,35 +5,32 @@ document.getElementById("sentimiento").onkeyup = () => {
     document.getElementById("sentimiento").style.color = `rgb(${red}, ${green}, ${blue})`;
 }
 
-function ejercicio_1(){
+function ejercicio_1() {
     // Generación de una tabla con números, cuadrados y cubos:
     var numeroStr = prompt("Ingresa un número");
-    var numero = parseInt(numeroStr); 
+    var numero = parseInt(numeroStr);
 
-    var ejercicio1Result = ""; // Crear una variable para almacenar el resultado de ejercicio 1
-    ejercicio1Result += "<p>Respuesta al ejercicio 1:</p>"; // Agregar texto antes de la tabla
+    var ejercicio1Result = "";
+    ejercicio1Result += "<p>Respuesta al ejercicio 1:</p>";
     ejercicio1Result += "<table>";
     ejercicio1Result += "<tr> <th> Número  </th> <th> Cuadrado  </th> <th> Cubo </th> </tr>";
-    for(var i = 1; i <= numero; i++){
-        ejercicio1Result += "<tr> <td>" + i + "</td> <td>" + Math.pow(i,2) + "</td> <td>" + Math.pow(i,3) + "</td> </tr>";
-    } 
+    for (var i = 1; i <= numero; i++) {
+        ejercicio1Result += "<tr> <td>" + i + "</td> <td>" + Math.pow(i, 2) + "</td> <td>" + Math.pow(i, 3) + "</td> </tr>";
+    }
     ejercicio1Result += "</table>";
-    document.getElementById("ejercicio_1").innerHTML = "Respuesta al ejercicio 1";
-    document.getElementById("ejercicio_1").innerHTML = ejercicio1Result; // Mostrar el resultado de ejercicio 1
-    
+    document.getElementById("ejercicio_1").innerHTML = ejercicio1Result;
 }
 
-function ejercicio_2(){
-    //Suma de 2 numeros aleatorios:
+function ejercicio_2() {
     var num1 = Math.floor(Math.random() * 10) + 1;
     var num2 = Math.floor(Math.random() * 10) + 1;
- 
+
     var sumaEsperada = num1 + num2;
- 
+
     var respuesta = prompt("¿Cuánto es " + num1 + " + " + num2 + "?");
- 
+
     var respuestaNumero = parseInt(respuesta);
- 
+
     if (respuestaNumero === sumaEsperada) {
         document.getElementById("resultado").innerHTML = "¡Correcto! La respuesta es " + sumaEsperada;
     } else {
@@ -42,6 +39,37 @@ function ejercicio_2(){
     document.getElementById("ejercicio_2").innerHTML = "Respuesta al ejercicio 2";
 }
 
-// Llamamos primero al ejercicio 1 y luego al ejercicio 2
+function ejercicio_3(arr) {
+    var negativos = 0;
+    var ceros = 0;
+    var positivos = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            negativos++;
+        } else if (arr[i] === 0) {
+            ceros++;
+        } else {
+            positivos++;
+        }
+    }
+
+    var ejercicio3Result = "<p>Respuesta al ejercicio 3:</p>";
+    ejercicio3Result += "Negativos: " + negativos + ", Ceros: " + ceros + ", Positivos: " + positivos;
+
+    document.getElementById("ejercicio_3").innerHTML = ejercicio3Result;
+    
+    return {
+         negativos,
+         ceros,
+         positivos
+    };
+}
+
+// Ejemplo de uso
+const numeros = [-2, 0, 5, -1, 0, 3, 7];
+
+// Llamamos primero al ejercicio 1, luego al ejercicio 2 y finalmente al ejercicio 3
 ejercicio_1();
 ejercicio_2();
+ejercicio_3(numeros);
